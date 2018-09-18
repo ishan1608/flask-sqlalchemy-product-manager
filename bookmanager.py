1d1a2397720b1c78e9fe302f5306d770a87e9e93
Ishan <ishan1608@gmail.com>
2018-09-18 23:18:06 +0530

    configured sqlite

--- bookmanager.py
+++ bookmanager.py
@@ -1,6 +1,15 @@
+import os
+
 from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+
+project_dir = os.path.dirname(os.path.abspath(__file__))
+database_file = 'sqlite:///{}'.format(os.path.join(project_dir, 'book_database.db'))
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = database_file
+
+db = SQLAlchemy(app)
 
 
 @app.route('/', methods=['GET', 'POST', 'OPTIONS'])
