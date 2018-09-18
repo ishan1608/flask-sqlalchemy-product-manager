ce1f119c497659e66bd445601c833e9d9b2ffddd
Ishan <ishan1608@gmail.com>
2018-09-19 00:22:31 +0530

    added a auto incrementing id to the model

--- bookmanager.py
+++ bookmanager.py
@@ -21,7 +21,8 @@ db = SQLAlchemy(app)
 
 
 class Book(db.Model):
-    title = db.Column(db.String(128), unique=True, nullable=False, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(128), unique=True, nullable=False)
 
     def __repr__(self):
         return '<Title: {}>'.format(self.title)
