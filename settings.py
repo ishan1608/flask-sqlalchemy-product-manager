c5725eeb0dac0e96322d3937872a4293a6f535fc
Ishan <ishan1608@gmail.com>
2018-09-25 23:50:34 +0530

    selecting a specific database with redis

--- settings.py
+++ settings.py
@@ -13,8 +13,8 @@ DATABASE_URL = (
 )
 
 CELERY = {
-    'CELERY_BROKER_URL': 'redis://localhost:6379',
-    'CELERY_RESULT_BACKEND': 'redis://localhost:6379',
+    'CELERY_BROKER_URL': 'redis://localhost:6379/0',
+    'CELERY_RESULT_BACKEND': 'redis://localhost:6379/0',
 }
 
 CELERY_ENABLED = True
